e330c7743351265bc2a69ae5850e70fc47880cb5
rohith kumar gowda h n <rohithkumargowdahn@gmai.com>
2021-09-13 22:49:05 +0530

    hours or salary reached

---
+++ Calculate_day_wage_reached.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -x
+
+echo "Welcome To Employee Wage Compution"
+WAGE_PER_HOUR=20
+IS_FULL_TIME=2
+IS_PART_TIME=1
+EMPLOYEE_HOUR_FULLTIME=8
+EMPLOYEE_HOUR_PARTTIME=4
+read -p "To number of working day :" NUMBER_OF_WORKING_DAYS
+read -p "To number of working hours :" NUMBER_OF_WORKING_HOUR
+totalSalary=0
+totalEmployeeHours=0
+totalWorkingDays=0
+while [[ $totalEmployeeHours -le $NUMBER_OF_WORKING_HOURS &&
+			$totalWorkingDays -le $NUMBER_OF_WORKING_DAYS ]]
+do
+	((totalWorkingDays++))
+	randomShiftCheck=$((RANDOM%3))
+	case $randomShiftCheck in
+
+		$IS_FULL_TIME )
+			employeeHour=$((EMPLOYEE_HOUR_FULLTIME))
+			;;
+		$IS_PART_TIME )
+			employeeHour=$((EMPLOYEE_HOUR_PARTTIME))
+			;;
+		* )
+			employeeHour=0
+			;;
+	esac
+	totalEmployeeHours=$(($totalEmployeeHours + $employeeHour))
+	echo "To total employee hours :" $totalEmployeeHours	
+done
+
+totalWorkingWage=$(($totalEmployeeHours * $WAGE_PER_HOUR))
